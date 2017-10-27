fba2782c82b552f27915160ad13fb11f17d927e1
Rohan Garg <rohgarg@ccs.neu.edu>
2017-10-27 17:00:58 -0400

    cuda-test4: minor clean up

--- contrib/cuda/test/test4.cu
+++ contrib/cuda/test/test4.cu
@@ -7,19 +7,22 @@ __device__ int incr = 10;
 
 __global__ void add(int a, int b, int *c)
 {
-	*c = a+b+incr++;
+	*c = *c+a+b+incr++;
 }
 
 int main(int argc, char **argv)
 {
 	// test
 	int a = 2, b = 3, c;
+	int t = 0;
 	int *cuda_c = NULL;
 
 	cudaMalloc(&cuda_c, sizeof(int));
-        sleep(10);
+	cudaMemcpy(cuda_c, &t, sizeof(int), cudaMemcpyHostToDevice);
+	sleep(10);
 	add<<<1,1>>>(a, b, cuda_c);
-    add<<<1,1>>>(a, b, cuda_c);
+	add<<<1,1>>>(a, b, cuda_c);
+	cudaDeviceSynchronize();
 	cudaMemcpy(&c, cuda_c, sizeof(int), cudaMemcpyDeviceToHost);
 	cudaFree(cuda_c);
 
