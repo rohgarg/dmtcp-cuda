b1f4b630a131c9b18e10280bb3df03992df814d8
Rohan Garg <rohgarg@ccs.neu.edu>
2018-02-06 10:49:10 -0800

    cuda-plugin: clean up remnants of old code

--- contrib/cuda/cuda-common-utils.cpp
+++ contrib/cuda/cuda-common-utils.cpp
@@ -11,11 +11,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#if USE_SHM
-# include <sys/ipc.h>
-# include <sys/shm.h>
-#endif
-
 // DMTCP utils
 #include "constants.h"
 #include "processinfo.h"
@@ -23,9 +18,6 @@
 #include "cuda_plugin.h"
 
 int initialized = False;
-#if USE_SHM
-void *shmaddr = NULL;
-#endif
 
 // for pinned memory
 // #define PINNED_MEM_MAX_ALLOC 100
@@ -78,18 +70,6 @@ proxy_initialize(void)
              .Text("Failed to exec cudaproxy");
   }
 
-#if USE_SHM
-  // create shared memory
-  key_t shmKey;
-  JASSERT((shmKey = ftok(".", 1) != -1)(JASSERT_ERRNO);
-
-  int shm_flags = IPC_CREAT | 0666;
-  int shmID;
-  JASSERT((shmID = shmget(shmKey, SHMSIZE, shm_flags)) != -1)(JASSERT_ERRNO);
-
-  JASSERT((shmaddr = shmat(shmID, NULL, 0)) != (void *)-1)(JASSERT_ERRNO);
-#endif
-
   // connect to the proxy:server
   JASSERT((skt_master = socket(AF_UNIX, SOCK_STREAM, 0)) > 0)(JASSERT_ERRNO);
 
@@ -103,11 +83,6 @@ proxy_initialize(void)
     }
   }
 
-#if USE_SHM
-  // Send the shmID to the proxy
-  int realId = dmtcp_virtual_to_real_shmid(shmID);
-  JASSERT(write(skt_master, &realId, sizeof(shmID)) != -1)(JASSERT_ERRNO);
-#endif
 #ifdef USE_CMA
   JASSERT(readAll(skt_master, &cpid, sizeof(cpid)) == sizeof(cpid) && cpid > 0);
 #endif // ifdef USE_CMA
@@ -161,114 +136,6 @@ writeAll(int fd, const void *buf, size_t count)
   } while (num_written < count);
   JASSERT(num_written == count) (num_written) (count);
   return num_written;
-}
-
-#ifndef PYTHON_AUTO_GENERATE
-    // Old non-auto-generated version
-// open the log file and
-// append a cuda system call structure to it
-void log_append(cudaSyscallStructure record)
-{
-  printf("**** OBSOLETE:  Please re-write to use log/replay style from"
-         "log_append_and_read.cpp\n");
-  JASSERT(write(logFd, &record, sizeof(record)) != -1)(JASSERT_ERRNO);
-}
-
-bool log_read(cudaSyscallStructure *record)
-{
-  printf("**** OBSOLETE:  Please re-write to use log/replay style from"
-         "log_append_and_read.cpp\n");
-  int ret = read(logFd, record, sizeof(*record));
-  if (ret == -1) {
-    JASSERT(false)(JASSERT_ERRNO);
-  }
-  if (ret == 0 || ret < sizeof(*record)) {
-    return false;
-  }
-  return true;
-}
-#else
-// The previous design (not using auto-generated code) required
-//   cudaSyscallStructure *record
-// where the struct would be a union of all possible structs for any CUDA call.
-// In the new design, we simply use a static buffer of sufficient size,
-//   LOG_READ_BUF_SIZE.
-
-// open the log file and
-// append a cuda system call structure to it
-// RECOMMENDED USAGE FOR WRITING LAST LOG VALUE BEFORE CKPT:
-//    enum cuda_op tmp = OP_LAST_FNC;
-//    log_append(&tmp, sizeof tmp);
-void log_append(void *ptr, size_t size) {
-  JASSERT(write(logFd, &size, sizeof(size)) != sizeof(size))(JASSERT_ERRNO);
-  JASSERT(write(logFd, ptr, size) != size)(JASSERT_ERRNO);
-}
-
-#define LOG_READ_BUF_SIZE 1000
-static int log_read_buf[LOG_READ_BUF_SIZE]; // Assumes that 1000 bytes suffices
-                                     // as long as we don't replay cudaMalloc.
-
-// NOTE: Caller must use result buffer, before calling log_read() again.
-// RECOMMENDED USAGE FOR REPLAY TO PROXY:
-//    size_t size;
-//    void *buf = log_read(&size);
-//    if ((*(enum cuda_op *)buf) == OP_LAST_FNC) {
-//      return; // Nothing more exists in the log.
-//    }
-//    write(skt_master, buf, size);  // Replay it in the proxy.
-void * log_read(size_t *size) { // size is an "out" parameter
-  JASSERT(size != NULL);
-  JASSERT(read(logFd, size, sizeof(*size)) != sizeof(*size))(JASSERT_ERRNO);
-  JASSERT(*size <= LOG_READ_BUF_SIZE);
-  JASSERT(read(logFd, log_read_buf, *size) != *size)(JASSERT_ERRNO);
-  return log_read_buf;
-}
-#endif
-
-/*
-  This function sends to the proxy the structure with cuda syscall parameters.
-  It then receives the return value and gets the structure back.
-*/
-void send_recv(int fd, cudaSyscallStructure *strce_to_send,
-               cudaSyscallStructure *rcvd_strce, cudaError_t *ret_val)
-{
-  // send the structure
-  JASSERT(write(fd, strce_to_send, sizeof(cudaSyscallStructure)) != -1)
-         (JASSERT_ERRNO);
-
-  if (strce_to_send->payload) {
-    JASSERT(write(fd, strce_to_send->payload, strce_to_send->payload_size) !=
-            -1)(strce_to_send->payload)(JASSERT_ERRNO);
-  }
-
-  // receive the result
-  JASSERT(read(fd, ret_val, sizeof(int)) != -1)(JASSERT_ERRNO);
-
-
-  if (strce_to_send->op != CudaGetLastError){
-    JASSERT((*(cudaError_t*)ret_val) == cudaSuccess)
-       (cudaGetErrorString(*(cudaError_t*)ret_val)).Text("CUDA syscall failed");
-  }
-
-  // get the structure back
-  memset(rcvd_strce, 0, sizeof(cudaSyscallStructure));
-  JASSERT(read(fd, rcvd_strce, sizeof(cudaSyscallStructure)) != -1)
-         (JASSERT_ERRNO);
-
-  switch(rcvd_strce->op)
-  {
-    case CudaGetErrorString:
-    {
-      // "cudaGetErrorString" is a special case, its return type
-      // is "const char *".
-      size_t size = (rcvd_strce->syscall_type).cuda_get_error_string.size;
-      char *error_string = (char *) malloc(size * sizeof(char));
-      JASSERT(read(fd, error_string, size) != -1) (JASSERT_ERRNO);
-      (rcvd_strce->syscall_type).cuda_get_error_string.error_string \
-       = error_string;
-    }
-    break;
-  }
 }
 
 void
--- contrib/cuda/cuda_plugin.h
+++ contrib/cuda/cuda_plugin.h
@@ -9,6 +9,8 @@
 #include <cublas.h>
 #include <sys/time.h>
 
+#include "dmtcp.h"
+
 // The following declarations, definitions, etc. are common
 // to the DMTCP CUDA plugin and the CUDA proxy.
 
@@ -64,6 +66,8 @@ extern std::map<uint64_t, uint64_t> uvmWriteReqMap;
 #endif // ifdef STATS
 
 extern void print_stats();
+EXTERNC ssize_t readAll(int fd, void *buf, size_t count);
+EXTERNC ssize_t writeAll(int fd, const void *buf, size_t count);
 
 #ifndef STANDALONE
 
@@ -73,41 +77,15 @@ extern void print_stats();
 #include <stdint.h>
 
 #include "jassert.h"
-#include "dmtcp.h"
 #include "dmtcp_dlsym.h"
 
-#define DEBUG_SIGNATURE "[CUDA Plugin]"
-#ifdef CUDA_PLUGIN_DEBUG
-# define DPRINTF(fmt, ...) \
-  do { fprintf(stderr, DEBUG_SIGNATURE fmt, ## __VA_ARGS__); } while (0)
-#else // ifdef CUDA_PLUGIN_DEBUG
-# define DPRINTF(fmt, ...) \
-  do {} while (0)
-#endif // ifdef CUDA_PLUGIN_DEBUG
-
-
 #define   _real_fork      NEXT_FNC_DEFAULT(fork)
-
-#define   _real_cudaMalloc      NEXT_FNC_DEFAULT(cudaMalloc)
-#define   _real_cudaMemcpy      NEXT_FNC_DEFAULT(cudaMemcpy)
-#define   _real_dlopen          NEXT_FNC_DEFAULT(dlopen)
-#define   _real_dlclose         NEXT_FNC_DEFAULT(dlclose)
-#define   _real_dlsym           NEXT_FNC_DEFAULT(dlsym)
-#define   _real_cuLaunchKernel  NEXT_FNC_DEFAULT(cuLaunchKernel)
-
-#define   _real_cudaConfigureCall     NEXT_FNC_DEFAULT(cudaConfigureCall)
-#define   _real_cudaLaunch            NEXT_FNC_DEFAULT(cudaLaunch)
-#define   _real_cudaFuncGetAttributes NEXT_FNC_DEFAULT(cudaFuncGetAttributes)
-#define   _real_cudaSetupArgument     NEXT_FNC_DEFAULT(cudaSetupArgument)
-#define   _real_cudaLaunchKernel      NEXT_FNC_DEFAULT(cudaLaunchKernel)
 
 #define True    1
 #define False   0
 
 #define SKTNAME "proxy"
 #define LOGFILE "cudaSysCallsLog"
-
-#define SHMSIZE 128
 
 extern int initialized;
 extern int ufd_initialized;
@@ -125,374 +103,6 @@ extern int skt_master;
 extern struct sockaddr_un sa_proxy;
 
 extern bool enableCudaCallLogging;
-#endif // ifndef STANDALONE
-
-enum cuda_syscalls
-{
-  CudaMalloc,
-  CudaMallocPitch,
-  CudaMallocManaged,
-  CudaMallocManagedMemcpy,
-  CudaFree,
-  CudaMallocArray,
-  CudaFreeArray,
-  CudaMemcpy,
-  CudaHostAlloc,
-  CudaConfigureCall,
-  CudaSetupArgument,
-  CudaLaunch,
-  CudaThreadSync,
-  CudaGetLastError,
-  CudaGetErrorString,
-  CudaMemcpy2D,
-  CudaDeviceReset,
-  CudaMemcpyToSymbol,
-  CudaCreateChannelDesc,
-  CudaBindTexture2D,
-  /*-- Added to support HPGMG-CUDA */
-  CudaBindTexture,
-  CudaCreateTextureObject,
-  CudaPeekAtLastError,
-  CudaProfilerStart,
-  CudaProfilerStop,
-  CudaStreamSynchronize,
-  CudaUnbindTexture,
-  CudaDestroyTextureObject,
-  CudaEventDestroy,
-  CudaEventQuery,
-  CudaFreeHost,
-  CudaDeviceCanAccessPeer,
-  CudaDeviceGetAttribute,
-  CudaDeviceSetCacheConfig,
-  CudaDeviceSetSharedMemConfig,
-  CudaDeviceSynchronize,
-  CudaEventCreateWithFlags,
-  CudaEventRecord,
-  CudaFuncGetAttributes,
-  CudaGetDevice,
-  CudaGetDeviceCount,
-  CudaGetDeviceProperties,
-  CudaMallocHost, // to be revisited
-  CudaMemcpyAsync, // to be revisited
-  CudaMemset,
-  CudaMemsetAsync, // to be revisited
-  CudaSetDevice, // here
-  CudaPointerGetAttributes,
-  CudaOccupancyMaxActiveBlocksPerMultiprocessor
-};
-
-
-// the structure for all our cuda system calls
-// so far it's for the following functions
-// cudaMalloc, cudaMallocArray, cudaFree, and cudaMemcpy
-typedef struct
-{
-  enum cuda_syscalls op;
-  union
-  {
-    struct
-    {
-      // the structure takes a deferenced pointer
-      // Since it's the proxy that calls cudaMalloc()
-      // &pointer, (void **), will then be passed to cudaMalloc.
-      void *pointer;
-      size_t size;
-    } cuda_malloc;
-
-    struct
-    {
-      void *pointer;
-    } cuda_free;
-
-    struct
-    {
-      // the structure takes a deferenced pointer
-      // Since it's the proxy that calls cudaMallocArray()
-      // &array (cudaArray **) will then be passed to cudaMalloc.
-      struct cudaArray *array;
-      // it's the proxy that will pass &desc to cudaMallocArray()
-      struct cudaChannelFormatDesc desc;
-      size_t width;
-      size_t height;
-      unsigned int flags;
-    } cuda_malloc_array;
-
-    struct
-    {
-      struct cudaArray *array;
-    } cuda_free_array;
-
-    struct
-    {
-      void *destination;
-      const void *source;
-      size_t size;
-      enum cudaMemcpyKind direction;
-    } cuda_memcpy;
-
-    struct
-    {
-      void *destination;
-      const void *source;
-      size_t size;
-      enum cudaMemcpyKind direction;
-    } cuda_managed_memcpy;
-
-//    struct
-//    {
-//      // master and proxy will have different pointer to the shared memory
-//      // we only include pHost in this structure for record purpose
-//      void *pHost;
-//      size_t size;
-//      unsigned int flags;
-//    } cuda_host_alloc
-
-    struct
-    {
-      int gridDim[3];    // to mimic dim3
-      int blockDim[3];   // to mimic dim3
-      size_t sharedMem;
-      cudaStream_t stream;
-    } cuda_configure_call;
-
-    struct
-    {
-      const void *arg; // it's used for record.
-      size_t size;
-      size_t offset;
-    } cuda_setup_argument;
-
-    struct
-    {
-      const void *func_addr;
-    } cuda_launch;
-
-//    struct
-//    {
-//      const void *func;
-//    } cuda_launch_record;
-    struct
-    {
-      cudaError_t errorCode;
-      // return value non-trivial (i.e. other than cudaError_t)
-      char *error_string;
-      size_t size;
-    } cuda_get_error_string;
-
-    struct
-    {
-      // the structure takes a deferenced pointer
-      // Since it's the proxy that calls cudaMallocPitch()
-      // &devPtr, (void *), will then be passed to cudaMallocPitch.
-      void* devPtr;
-      // original argument is "*pitch" but this makes information
-      // exchange between wrapper and proxy easier.
-      size_t pitch;
-      size_t *pitchp;
-      size_t width;
-      size_t height;
-    } cuda_malloc_pitch;
-
-    struct
-     {
-       void * dst;
-       size_t dpitch;
-       const void * src;
-       size_t spitch;
-       size_t width;
-       size_t height;
-       enum cudaMemcpyKind kind;
-     } cuda_memcpy2_d;
-
-     struct
-     {
-       const void * symbol;
-       const void * src;
-       size_t count;
-       size_t offset;
-       enum cudaMemcpyKind kind;
-     } cuda_memcpy_to_symbol;
-
-     struct
-     {
-       int x;
-       int y;
-       int z;
-       int w;
-       enum cudaChannelFormatKind f;
-       // return value non-trivial (i.e. other than cudaError_t)
-       cudaChannelFormatDesc ret_val;
-     } cuda_create_channel_desc;
-
-     struct
-     {
-       // (*offset) is an "out" parameter. The value changes in the proxy.
-       // we chose "offset" instead of "*offset", it's easier to code this way.
-       size_t offset;
-       struct textureReference *texref;
-       const void  *devPtr;
-       // we chose "desc" instead of "*desc", it's easier to code this way.
-       struct cudaChannelFormatDesc desc;
-       size_t width;
-       size_t height;
-       size_t pitch;
-     } cuda_bind_texture2_d;
-
-     /* Added to support HPGMG-CUDA */
-     struct
-     {
-       size_t offset; // (*offset)
-       size_t *offsetp; // to be used on restart.
-       const textureReference *texref;
-       const void *devPtr;
-       struct cudaChannelFormatDesc desc; // (*desc)
-       size_t size;
-     } cuda_bind_texture;
-
-    struct
-    {
-      /* (* pTexObject) is an "out" parameter. */
-      cudaTextureObject_t pTexObject;
-      cudaTextureObject_t *pTexObjectp; // useful on restart.
-      /* (* pResDesc) will be passed as pointer in the proxy*/
-      struct cudaResourceDesc pResDesc;
-      /* (* pTexDesc) will be passed as pointer in the proxy*/
-      struct cudaTextureDesc pTexDesc;
-      /* (*pResViewDesc) will be passed as pointer in the proxy*/
-      struct cudaResourceViewDesc pResViewDesc;
-    } cuda_create_texture_object;
-
-    struct
-    {
-      cudaTextureObject_t texObject;
-    }cuda_destroy_texture_object;
-
-    struct
-    {
-      cudaStream_t stream;
-    } cuda_stream_synchronize;
-
-    struct
-    {
-      cudaEvent_t event;
-    } cuda_event_destroy;
-
-    struct
-    {
-      const textureReference* texref;
-    } cuda_unbind_texture;
-
-    struct
-    {
-      cudaEvent_t event;
-    } cuda_event_query;
-
-    struct
-    {
-      void *ptr;
-    } cuda_free_host;
-
-    struct
-    {
-      int canAccessPeer; // int instead of (int *) \
-                            since the value changes in the proxy.
-      int device;
-      int peerDevice;
-    } cuda_device_can_access_peer;
-
-    struct
-    {
-      int value; // int instead of (int *) since the value changes in the proxy.
-      cudaDeviceAttr attr;
-      int device;
-    }cuda_device_get_attribute;
-
-    struct
-    {
-      cudaFuncCache cacheConfig;
-    } cuda_deviceSetCacheConfig;
-
-    struct
-    {
-      cudaSharedMemConfig config;
-    } cuda_deviceSetSharedMemConfig;
-
-    struct
-    {
-      cudaEvent_t event; // event instead of (event *) since
-                         // the value changes in the proxy.
-      unsigned int flags;
-    } cuda_eventCreateWithFlags;
-
-    struct
-    {
-      cudaEvent_t event;
-      cudaStream_t stream;
-    } cuda_eventRecord;
-
-    struct
-    {
-      cudaFuncAttributes attr; // attr instead of (* attr) since the value
-                               // changes in the proxy.
-      const void *func;
-    } cuda_funcGetAttributes;
-
-    struct
-    {
-      int device; // int instead of (int *) size the value changes in the proxy.
-    } cuda_getDevice;
-
-    struct
-    {
-      int count; // int instead of (int *) size the value changes in the proxy.
-    } cuda_getDeviceCount;
-
-    struct
-    {
-      cudaDeviceProp prop; // the value changes in the proxy.
-      int device;
-    } cuda_getDeviceProperties;
-
-    struct
-    {
-      void *dst;
-      const void *src;
-      size_t count;
-      cudaMemcpyKind kind;
-      cudaStream_t stream;
-    } cuda_memcpyAsync;
-
-    struct
-    {
-      void *devPtr;
-      int value;
-      size_t count;
-    } cuda_memset;
-
-    struct
-    {
-      int  device;
-    } cuda_setDevice;
-
-    struct
-    {
-      void *ptr;
-      cudaPointerAttributes attributes;
-    } cuda_pointer_get_attributes;
-
-    struct
-    {
-      int numBlocks;
-      const void* func;
-      int  blockSize;
-      size_t dynamicSMemSize;
-    } cuda_occupancy_max_active_blocks_per_multiprocessor;
-  }syscall_type;
-  const void *payload;
-  size_t payload_size;
-} cudaSyscallStructure;
-
-#ifndef STANDALONE
 
 static inline void*
 getAlignedAddress(uintptr_t ptr, size_t alignment)
@@ -505,13 +115,6 @@ void proxy_initialize(void);
 void copy_data_to_host(void);
 void copy_data_to_device(void);
 
-#ifndef PYTHON_AUTO_GENERATE
-void send_recv(int fd, cudaSyscallStructure *strce_to_send,
-              cudaSyscallStructure *rcvd_strce, cudaError_t *ret_val);
-void log_append(cudaSyscallStructure record);
-bool log_read(cudaSyscallStructure *record);
-#else
-
 struct CudaCallLog_t {
   void *fncargs;
   size_t size;
@@ -520,16 +123,10 @@ struct CudaCallLog_t {
   void *host_addr;   // Valid only for a cudaMalloc region
 };
 
-EXTERNC ssize_t readAll(int fd, void *buf, size_t count);
-EXTERNC ssize_t writeAll(int fd, const void *buf, size_t count);
-
 // Just for testing purpose. Otherwise this function is obsolete.
-void send_recv(int fd, cudaSyscallStructure *strce_to_send,
-              cudaSyscallStructure *rcvd_strce, cudaError_t *ret_val);
 void logs_read_and_apply(void (*apply)(CudaCallLog_t *l));
 void log_append(void *ptr, size_t size,
                 void *results, size_t resSize);
-#endif
 
 struct ShadowRegion {
   void *addr;
@@ -569,12 +166,6 @@ void unregister_all_pages();
 void register_all_pages();
 void protect_all_pages();
 void flushDirtyPages();
-
-// This is now static, scope within one file
-// dmtcp::map<void*, void*>& shadowPageMap()fdef PYTHON_AUTO_GENERATE
-//  askdaj aslkdas;
-//  #endif
-//  ;
 
 #endif // ifndef STANDALONE
 
